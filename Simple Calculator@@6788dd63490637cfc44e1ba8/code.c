49014030a188173ecd020743ff488858aab800c1
Chahat <gupta.chahat641@gmail.com>
2025-01-24 22:50:57 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -2,18 +2,18 @@
 int main() {
     double a,b;
     char c;
-    scanf("%.1f %.1f %c",&a,&b,&c);
+    scanf("%f %f %c",&a,&b,&c);
     if (c=='+') {
-        printf("%.1f",a+b);
+        printf("%.0f",a+b);
     }
     else if (c=='-') {
-        printf("%.1f",a-b);
+        printf("%.0f",a-b);
     }
     else if (c='*') {
-        printf("%.1f",a*b);
+        printf("%.0f",a*b);
     }
     else if (c=='/') {
-        printf("%.1f",a/b);
+        printf("%.0f",a/b);
     }
     else {
         printf("Error");
