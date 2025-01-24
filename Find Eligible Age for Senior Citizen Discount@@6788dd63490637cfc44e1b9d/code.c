2faa9ce6f111b2d5f16e8f489a20a11cec4c7b18
Chahat <gupta.chahat641@gmail.com>
2025-01-24 11:36:57 +0530

    Solution to Find Eligible Age for Senior Citizen Discount

---
+++ Find Eligible Age for Senior Citizen Discount@@6788dd63490637cfc44e1b9d/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int age;
+    scanf("%d",&age);
+    if (age>60) {
+        printf("Not Eligible");
+    }
+    else {
+        printf("Eligible");
+    }
+}
