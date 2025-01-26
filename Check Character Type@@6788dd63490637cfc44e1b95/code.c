29ff59e3942d69bb43a74d3b3a145edf087ab6b5
Chahat <gupta.chahat641@gmail.com>
2025-01-26 13:18:01 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -4,16 +4,26 @@ int main() {
     scanf("%c",&a);
     if ((a>='a' && a<='z')||(a>='A' && a<='Z')){
         switch (a) {
-            case'a':printf("Vowel");break;
-            case'e':printf("Vowel");break;
-            case'i':printf("Vowel");break;
-            case'o':printf("Vowel");break;
-            case'u':printf("Vowel");break;
-            case'A':printf("Vowel");break;
-            case'E':printf("Vowel");break;
-            case'I':printf("Vowel");break;
-            case'O':printf("Vowel");break;
-            case'U':printf("Vowel");break;
+            case'a':printf("Vowel");
+                    break;
+            case'e':printf("Vowel");
+                    break;
+            case'i':printf("Vowel");
+                    break;
+            case'o':printf("Vowel");
+                    break;
+            case'u':printf("Vowel");
+                    break;
+            case'A':printf("Vowel");
+                    break;
+            case'E':printf("Vowel");
+                    break;
+            case'I':printf("Vowel");
+                    break;
+            case'O':printf("Vowel");
+                    break;
+            case'U':printf("Vowel");
+                    break;
             default:printf("Consonant");
         }
     }
